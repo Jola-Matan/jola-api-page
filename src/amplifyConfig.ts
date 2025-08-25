c87d9111b780e7071405b42b17f240d4c9c114ff
Jola-Matan <matan@heyjola.com>
2025-08-25 19:42:51 +0300

    Update callback and login URIs for onboarding

--- src/amplifyConfig.ts
+++ src/amplifyConfig.ts
@@ -3,11 +3,11 @@ const isLocalhost = window.location.hostname === 'localhost';
 
 const redirectUri = isLocalhost
   ? 'http://localhost:5173/callback'
-  : 'https://app.heyjola.com/callback';
+  : 'https://onboarding.heyjola.com/callback';
 
 const logoutUri = isLocalhost
   ? 'http://localhost:5173/login'
-  : 'https://app.heyjola.com/login'; 
+  : 'https://onboarding.heyjola.com/login'; 
 
 const amplifyConfig = {
   clientId: '55018cv7221fsmjst02e54f55a',
